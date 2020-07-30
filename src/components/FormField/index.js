ae64df612b48293566eff4efadc6b6a67d4c86b8
SoraAsc <soragg59@gmail.com>
2020-07-30 14:15:12 -0300

    validação e começo do back-end

--- src/components/FormField/index.js
+++ src/components/FormField/index.js
@@ -1,29 +1,53 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable react/prop-types */
+/* eslint-disable indent */
+/* eslint-disable react/jsx-indent-props */
+/* eslint-disable react/jsx-indent */
+/* eslint-disable linebreak-style */
+
 import React from 'react';
+import PropTypes from 'prop-types';
 import TextField from '@material-ui/core/TextField';
-import { withStyles } from "@material-ui/core/styles";
+import { withStyles } from '@material-ui/core/styles';
+
 const styles = {
     textfield: {
-        background: "white",
-        display: "block",
-        marginBottom: "5px",
-
+        background: 'white',
+        display: 'block',
+        marginBottom: '5px',
+        outline: '0',
+        border: '0',
+        borderTop: '4px solid transparent',
+        borderBottom: '4px solid white-smoke',
     },
     input: {
-        color: "black",
-        width: "50vh"
+        background: 'white',
+        color: 'black',
+        width: '80vw',
 
-    }
+    },
 };
 
-function FormField({ classes, label, type, name, value, onChange }) {
+function FormField({
+    classes, label, type, name, value, onChange,
+}) {
+    const fieldId = `id_${name}`;
 
     return (
-        <TextField className={classes.textfield} multiline name={name} value={value} label={label} variant="filled" type={type} onChange={onChange}
+        <TextField
+            className={classes.textfield}
+            multiline
+            name={name}
+            value={value}
+            type={type}
+            label={label}
+            variant="filled"
+            onChange={onChange}
+            id={fieldId}
             InputProps={{
-                className: classes.input
+                className: classes.input,
             }}
         />
-
         // <div>
         //     <label>
         //         {label}:
@@ -35,7 +59,22 @@ function FormField({ classes, label, type, name, value, onChange }) {
         //         />
         //     </label>
         // </div>
-    )
+    );
 }
+
+FormField.defaultProps = {
+    type: 'text',
+    value: '',
+    onChange: () => { },
+};
+
+FormField.prototype = {
+    classes: PropTypes.string.isRequired,
+    label: PropTypes.string.isRequired,
+    type: PropTypes.string,
+    name: PropTypes.string.isRequired,
+    value: PropTypes.string,
+    onChange: PropTypes.func,
+};
 
 export default withStyles(styles)(FormField);
--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -1,118 +1,118 @@
-import React, { useState } from 'react'
+/* eslint-disable linebreak-style */
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import PageDefault from '../../../components/PageDefault';
 import FormField from '../../../components/FormField';
-import Button from '../../../components/Button'
+import Button from '../../../components/Button';
 
 function CadastroCategoria() {
-    const valoresIniciais = {
-        nome: '',
-        descricao: '',
-        cor: '',
-    }
-    const [categorias, setCategorias] = useState([]);
-    const [values, setValues] = useState(valoresIniciais);
+  const valoresIniciais = {
+    nome: '',
+    descricao: '',
+    cor: '#ffffff',
+  };
+  const [categorias, setCategorias] = useState([]);
+  const [values, setValues] = useState(valoresIniciais);
 
-    function setValue(key, value) {
-        //chave: nome, descricao, cor
-        setValues({
-            ...values,
-            [key]: value, // nome: 'valor'
-        })
-    }
+  function setValue(key, value) {
+    // chave: nome, descricao, cor
+    setValues({
+      ...values,
+      [key]: value, // nome: 'valor'
+    });
+  }
 
-    function handleChange(infosDoEvento) {
-        const { value } = infosDoEvento.target;
-        setValue(
-            infosDoEvento.target.getAttribute('name'),
-            value
-        );
-    }
+  function handleChange(infosDoEvento) {
+    const { value } = infosDoEvento.target;
+    setValue(
+      infosDoEvento.target.getAttribute('name'),
+      value,
+    );
+  }
+  useEffect(() => {
+    const URL = 'http://localhost:8080/categorias';
+    fetch(URL)
+      .then(async (res) => {
+        const resposta = await res.json();
+        setCategorias([
+          ...resposta,
+        ]);
+      });
+  }, []);
+  return (
+    <PageDefault>
 
-    return (
-        <PageDefault >
+      <Link to="/">
+        Ir para home
+      </Link>
 
-            <Link to="/">
-                Ir para home
-            </Link>
+      <div style={{ textAlign: 'center' }}>
+        <h1>
+          Cadastro de Categoria:
+          {values.nome}
+        </h1>
+        <div style={{ display: 'inline-block', justifyContent: 'center' }}>
+          <form onSubmit={function handleSubmit(infosDoEvento) {
+            infosDoEvento.preventDefault();
+            setCategorias([
+              values,
+              ...categorias,
+            ]);
 
-            <div style={{ textAlign: 'center' }}>
-                <h1>Cadastro de Categoria: {values.nome}</h1>
-                <div style={{ display: "inline-block", justifyContent: "center" }}>
-                    <form onSubmit={function handleSubmit(infosDoEvento) {
-                        infosDoEvento.preventDefault();
-                        setCategorias([
-                            values,
-                            ...categorias
-                        ]);
+            setValues(valoresIniciais);
+          }}
+          >
 
-                        setValues(valoresIniciais)
-                    }}>
+            <FormField
+              label="Nome da Categoria"
+              type="text"
+              name="nome"
+              value={values.nome}
+              onChange={handleChange}
+            />
 
+            <FormField
+              label="Descrição"
+              type="number"
+              name="descricao"
+              value={values.descricao}
+              onChange={handleChange}
+            />
 
-                        <FormField
-                            label="Nome da Categoria"
-                            type="text"
-                            name="nome"
-                            value={values.nome}
-                            onChange={handleChange}
-                        />
-
-                        <FormField
-                            label="Descrição"
-                            type="text"
-                            name="descricao"
-                            value={values.descricao}
-                            onChange={handleChange}
-                        />
-                        {/* <div>
-                    <label>
-                    Descrição:
-                    <textarea
-                    type="text"
-                    name="descricao"
-                    value={values.descricao}
-                    onChange={handleChange}
-                    />
-                    </label>
-                </div> */}
-
-                        {/* <FormField
-                            label="Cor"
-                            type="color"
-                            name="cor"
-                            value={values.cor}
-                            onChange={handleChange}
-                        /> */}
-
-                        <div>
-                            <input style={{ width: "100%", marginBottom: "5px", background: 'none' }}
-                                type="color"
-                                name="cor"
-                                value={values.cor}
-                                onChange={handleChange}
-                            />
-                        </div>
-
-                        <Button style={{ color: 'black' }}>
-                            Cadastrar
-                        </Button>
-
-                    </form>
-                </div>
+            <div>
+              <input
+                style={{
+                  height: '40px', width: '100%', marginBottom: '5px', background: 'none',
+                }}
+                type="color"
+                name="cor"
+                value={values.cor}
+                onChange={handleChange}
+              />
             </div>
-            <ul>
-                {categorias.map((categoria, index) => {
-                    return (
-                        < li key={`${categoria} ${index}`}>
-                            {categoria.nome}
-                        </li>
-                    )
-                })}
-            </ul>
 
-        </PageDefault >
-    )
+            <Button style={{ color: 'black' }}>
+              Cadastrar
+            </Button>
+
+          </form>
+        </div>
+      </div>
+      {categorias.length === 0 && (
+      <div style={{ color: 'white' }}>
+        Loading...
+      </div>
+      )}
+      <ul>
+        {categorias.map((categoria) => (
+          <li key={`${categoria.nome}`}>
+            {categoria.nome}
+          </li>
+        ))}
+      </ul>
+
+    </PageDefault>
+  );
 }
 
 export default CadastroCategoria;
